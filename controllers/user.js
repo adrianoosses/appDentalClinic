3b4485eb9c0e46bbcff696fb1e1aa0cef16dc061
aosses <adrianoosses9@gmail.com>
2020-11-07 17:43:00 +0100

    Init login

--- controllers/user.js
+++ controllers/user.js
@@ -11,6 +11,27 @@ exports.getAllUsers = async (req, res) =>{
             res.status(500).send({
                 message: 'Problem getting appointments.'
             })
-        })
-        
+        })    
 }
+
+exports.login = async(req, res) =>{
+    let password = req.body.password;
+    let usrLoginString = await getUsersByBody(req, res);
+    let resul = false;
+    let msg = "";
+    if(usrLoginString !== undefined && usrLoginString !== null){
+        if(usrLoginString.pass === password){
+            console.log("Correct user and token. LOOGED");
+            msg = "Logged";
+            resul = true;
+        } else{
+            console.log("Wrong user or password ");
+            msg = "Not logged";
+            resul = false;
+        }
+    }else{
+        msg = "ERROR";
+    }
+    res.json({"msg":msg});
+    return resul;
+};
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const {getAllUsers} = require('../controllers/user.js');
 
 router.get('/getAll', getAllUsers);
+router.get('/login', login);
 
 
 exports.routes = router;
