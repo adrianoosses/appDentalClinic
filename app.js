4ccc8363c9b6dc2a273156dd1fa70233ed926a90
aosses <adrianoosses9@gmail.com>
2020-11-18 19:19:46 +0100

    header david

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 //CORS
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Email");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
 
