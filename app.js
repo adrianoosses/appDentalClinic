269b8c76c36f50282a32d0185571ebd0badf2bfa
aosses <adrianoosses9@gmail.com>
2020-11-19 10:01:10 +0100

    cors nuevo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const app = express();
 let ap = require('./routes/appointment.js');
 let us = require('./routes/usr.js');
-var cors = require('cors');
+let cors = require('cors');
 
 
 // asignacion del puerto 3000 para el servidor
@@ -12,13 +12,14 @@ app.use(express.json());
 
 //CORS
 app.use(cors());
+/*
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});
+}); */
 
 app.use('/appointment', ap.routes);
 app.use('/user', us.routes);
