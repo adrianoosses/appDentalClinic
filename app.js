3c413ad12cf3463f8ae91cde84548d12c55bc9cf
aosses <adrianoosses9@gmail.com>
2020-11-19 21:27:34 +0100

    cors profe

--- app.js
+++ app.js
@@ -11,16 +11,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 //CORS
-app.use(cors());
-/*
+//app.use(cors());
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, 
-    X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-}); */
+}); 
 
 app.use('/appointment', ap.routes);
 app.use('/user', us.routes);
